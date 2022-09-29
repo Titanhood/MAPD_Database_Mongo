87f85952f9155fc2310589c5443f3048bc95e6cc
Titanhood <ajay.srivastav0070@gmail.com>
2022-09-29 14:00:23 -0400

    GET working

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var plugin = function (options) {
     });
 
     seneca.add({ role: 'product', cmd: 'get' }, function (msg, respond) {
-        this.make('product').load$(msg.data.product_id, respond);
+        this.make('product').load$(msg.data.merch_id, respond);
     });
 
     seneca.add({ role: 'product', cmd: 'get-all' }, function (msg, respond) {
@@ -15,7 +15,7 @@ var plugin = function (options) {
     });
 
     seneca.add({ role: 'product', cmd: 'delete' }, function (msg, respond) {
-        this.make('product').remove$(msg.data.product_id, respond);
+        this.make('product').remove$(msg.data.merch_id, respond);
     });
 
 
@@ -29,12 +29,12 @@ var seneca = require("seneca")();
 seneca.use(plugin);
 seneca.use('seneca-entity');
 
-seneca.add('role:api, cmd:add-product', function (args, done) {
-    console.log("--> cmd:add-product");
+seneca.add('role:api, cmd:add-merch', function (args, done) {
+    console.log("--> products GET: recieved request");
     var product = {
-        product: args.firstname,
-        price: args.lastname,
-        category: args.position
+        product: args.product,
+        price: args.price,
+        category: args.category
     }
     console.log("--> product: " + JSON.stringify(product));
     seneca.act({ role: 'product', cmd: 'add', data: product }, function (err, msg) {
@@ -43,44 +43,44 @@ seneca.add('role:api, cmd:add-product', function (args, done) {
     });
 });
 
-seneca.add('role:api, cmd:get-all-products', function (args, done) {
-    console.log("--> cmd:get-all-products");
+seneca.add('role:api, cmd:get-all-merch', function (args, done) {
+    console.log("--> products GET: sending response");
     seneca.act({ role: 'product', cmd: 'get-all' }, function (err, msg) {
         console.log(msg);
         done(err, msg);
     });
 });
 
-seneca.add('role:api, cmd:get-product', function (args, done) {
-    console.log("--> cmd:get-product, args.product_id: " + args.product_id);
-    seneca.act({ role: 'product', cmd: 'get', data: { product_id: args.product_id } }, function (err, msg) {
+seneca.add('role:api, cmd:get-merch', function (args, done) {
+    console.log("--> cmd:get-merch, args.merch_id: " + args.merch_id);
+    seneca.act({ role: 'product', cmd: 'get', data: { merch_id: args.merch_id } }, function (err, msg) {
         console.log(msg);
         done(err, msg);
     });
 });
 
 
-seneca.add('role:api, cmd:delete-product', function (args, done) {
-    console.log("--> cmd:delete-product, args.product_id: " + args.product_id);
-    seneca.act({ role: 'product', cmd: 'delete', data: { product_id: args.product_id } }, function (err, msg) {
+seneca.add('role:api, cmd:delete-merch', function (args, done) {
+    console.log("--> cmd:delete-merch, args.merch_id: " + args.merch_id);
+    seneca.act({ role: 'product', cmd: 'delete', data: { merch_id: args.merch_id } }, function (err, msg) {
         console.log(msg);
         done(err, msg);
     });
 });
 
-seneca.add('role:api, cmd:delete-all-products', function (args, done) {
-    done(null, { cmd: "delete-all-products" });
+seneca.add('role:api, cmd:delete-all-merch', function (args, done) {
+    done(null, { cmd: "delete-all-merch" });
 });
 
 seneca.act('role:web', {
     use: {
-        prefix: '/product',
+        prefix: '/products',
         pin: { role: 'api', cmd: '*' },
         map: {
-            'add-product': { GET: true },
-            'get-all-products': { GET: true },
-            'get-product': { GET: true, },
-            'delete-product': { GET: true, }
+            'add-merch': { GET: true },
+            'get-all-merch': { GET: true },
+            'get-merch': { GET: true, },
+            'delete-merch': { GET: true, }
         }
     }
 })
@@ -92,10 +92,11 @@ app.use(seneca.export('web'));
 
 
 
-app.listen(3000)
+app.listen(3009)
 console.log("Server listening on localhost:3009 ...");
 console.log("----- Requests -------------------------");
-console.log("http://localhost:3009/product/add-product?product=Ajay&price=Shrivastav&category=Developer");
-console.log("http://localhost:3009/product/get-all-products");
-console.log("http://localhost:3009/product/get-product?product_id=1245");
-console.log("http://localhost:3009/product/delete-product?product_id=1245");
+console.log("http://localhost:3009/products/add-merch?product=Laptop&price=201.99&category=PC");
+console.log("http://localhost:3009/products/add-merch?product=MacBook&price=601.99&category=MAC");
+console.log("http://localhost:3009/products/get-all-merch");
+console.log("http://localhost:3009/products/get-merch?merch_id=1245");
+console.log("http://localhost:3009/products/delete-merch?merch_id=1245");
