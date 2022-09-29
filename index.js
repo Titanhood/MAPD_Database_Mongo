345b0c261d63f6d26e8881311412a2dabf55135e
Titanhood <ajay.srivastav0070@gmail.com>
2022-09-29 17:30:49 -0400

    GET tested and working with postman installed

--- index.js
+++ index.js
@@ -1,102 +1,37 @@
+var http = require('http');
+var fs = require('fs');
 
-var plugin = function (options) {
-    var seneca = this;
+var port = 3009;
+var host = '127.0.0.1';
+var filename = 'get-request.js';
 
-    seneca.add({ role: 'product', cmd: 'add' }, function (msg, respond) {
-        this.make('product').data$(msg.data).save$(respond);
-    });
-
-    seneca.add({ role: 'product', cmd: 'get' }, function (msg, respond) {
-        this.make('product').load$(msg.data.merch_id, respond);
-    });
-
-    seneca.add({ role: 'product', cmd: 'get-all' }, function (msg, respond) {
-        this.make('product').list$({}, respond);
-    });
-
-    seneca.add({ role: 'product', cmd: 'delete' }, function (msg, respond) {
-        this.make('product').remove$(msg.data.merch_id, respond);
-    });
+var requestCounter = 0;
 
 
-}
+var server = http.createServer(handleRequest);
 
-module.exports = plugin;
-
-
-
-var seneca = require("seneca")();
-seneca.use(plugin);
-seneca.use('seneca-entity');
-
-seneca.add('role:api, cmd:add-merch', function (args, done) {
-    console.log("--> products GET: recieved request");
-    var product = {
-        product: args.product,
-        price: args.price,
-        category: args.category
-    }
-    console.log("--> product: " + JSON.stringify(product));
-    seneca.act({ role: 'product', cmd: 'add', data: product }, function (err, msg) {
-        console.log(msg);
-        done(err, msg);
-    });
+server.listen(port, host, function () {
+    console.log("Server listening on: " + host + ":" + port);
 });
 
-seneca.add('role:api, cmd:get-all-merch', function (args, done) {
-    console.log("--> products GET: sending response");
-    seneca.act({ role: 'product', cmd: 'get-all' }, function (err, msg) {
-        console.log(msg);
-        done(err, msg);
-    });
-});
+function handleRequest(request, response) {    
+    requestCounter++;
+    console.log(">> Handling request... " + request.url );
+    console.log("   Request counter: " +  requestCounter);
 
-seneca.add('role:api, cmd:get-merch', function (args, done) {
-    console.log("--> cmd:get-merch, args.merch_id: " + args.merch_id);
-    seneca.act({ role: 'product', cmd: 'get', data: { merch_id: args.merch_id } }, function (err, msg) {
-        console.log(msg);
-        done(err, msg);
-    });
-});
-
-
-seneca.add('role:api, cmd:delete-merch', function (args, done) {
-    console.log("--> cmd:delete-merch, args.merch_id: " + args.merch_id);
-    seneca.act({ role: 'product', cmd: 'delete', data: { merch_id: args.merch_id } }, function (err, msg) {
-        console.log(msg);
-        done(err, msg);
-    });
-});
-
-seneca.add('role:api, cmd:delete-all-merch', function (args, done) {
-    done(null, { cmd: "delete-all-merch" });
-});
-
-seneca.act('role:web', {
-    use: {
-        prefix: '/products',
-        pin: { role: 'api', cmd: '*' },
-        map: {
-            'add-merch': { GET: true },
-            'get-all-merch': { GET: true },
-            'get-merch': { GET: true, },
-            'delete-merch': { GET: true, }
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    fs.readFile(filename, 'utf8', function (err, data) {
+        var requestCounterString = "======> Request Count: " + requestCounter + "\n======================================";
+        if (err) {
+            response.end(requestCounterString + err);
+            return console.log(err);
         }
-    }
-})
-
-var express = require('express');
-var app = express();
-app.use(require("body-parser").json())
-app.use(seneca.export('web'));
+        response.end(requestCounterString + data);
+        console.log(data);
+    })
+};
 
 
 
-app.listen(3009)
-console.log("Server listening on localhost:3009 ...");
-console.log("----- Requests -------------------------");
-console.log("http://localhost:3009/products/add-merch?product=Laptop&price=201.99&category=PC");
-console.log("http://localhost:3009/products/add-merch?product=MacBook&price=601.99&category=MAC");
-console.log("http://localhost:3009/products/get-all-merch");
-console.log("http://localhost:3009/products/get-merch?merch_id=1245");
-console.log("http://localhost:3009/products/delete-merch?merch_id=1245");
+
+
