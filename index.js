e793eb3c218b72add78891f954f16284c5188555
Titanhood <32667572+Titanhood@users.noreply.github.com>
2022-11-30 19:27:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ const routes = require('./routes/routes');
 app.use('/api', routes)
 
 app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+    console.log(`Server Started at ${8080}`)
 })
